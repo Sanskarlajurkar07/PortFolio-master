dc373f716bc517bd05b157de802744be2d1d3a43
Sanskarlajurkar07 <164156929+Sanskarlajurkar07@users.noreply.github.com>
2024-12-04 23:18:59 +0530

    Done !!

---
+++ src/components/About.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+
+function About() {
+  return (
+    <div
+      name="About"
+      className="max-w-screen-2xl container mx-auto px-4 md:px-20 my-20"
+    >
+      <div>
+        <h1 className="text-3xl font-bold mb-5">About</h1>
+        <p>
+        Hello, I'm Sanskar, a passionate Full Stack Developer with expertise in Java, React, and Spring Boot. With a background in advanced computing, I strive to create impactful and efficient software solutions that deliver exceptional user experiences.
+        </p>
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Education & Training
+        </h1>
+        <span>
+          <ul>
+        <li> Post Graduate Diploma in Advanced Computing (PG-DAC), CDAC Hyderabad, 2024,</li>
+       <li> B.E. in Electronics and Telecommunication Engineering
+        St. Vincent Pallotti College of Engineering & Technology, Nagpur | CGPA: 5.98 | Aug 2019 – Jul 2023 ,</li>
+        </ul>
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Skills & Expertise
+        </h1>
+        <span>
+          <ul>
+       <li> Proficient in: Java, JavaScript, React, Spring Boot, SQL, C++, C#</li>
+<li>Experienced with: REST APIs, Spring Security, Arduino, MySQL, and JPA</li>
+<li>Strong grasp of: Object-Oriented Programming (OOP), Data Structures, and Design Patterns</li>
+<li>Excellent problem-solving skills</li>
+<li>Effective communicator and team collaborator</li>
+</ul>
+
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Projects Experience
+        </h1>
+        <span>
+        <ol><b>Backend Developer (Project), CDAC Hyderabad, 2024</b>
+
+<p>        Developed efficient REST APIs for a Café Management System, streamlining user and order management functionalities.</p>
+</ol>
+<ol>
+<b>IoT Developer (Project), BE SVPTE College, 2024</b>
+
+<p>Designed and implemented a Smart Street Light System using Arduino, optimizing energy consumption.</p>
+</ol>
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Achievements &Certificationas
+        </h1>
+        <span>
+      	Completed Spring Boot with REST API, MVC, and Microservices (Udemy)
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Mission Statement
+        </h1>
+        <p>
+          MMy mission is to leverage my skills and creativity to build innovative software solutions that enhance user experiences and contribute positively to the digital landscape. I am committed to continuous learning and always seek new challenges to grow as a developer and problem-solver.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
